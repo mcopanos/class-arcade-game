826ea020418c1146bdea7ed8cdfbdf5ccd4a48a8
Mike Copanos <mcopanos@gmail.com>
2018-07-15 20:33:57 -0500

    version 1.0

---
+++ README.md
@@ -0,0 +1,23 @@
+Arcade Game
+===============================
+## Getting Started
+
+Welcome and thanks for wanting to play a mock-up of the classic frogger game. First things first, please feel free to clone or download the game files. To do so you will see a green rectangle button about 1/3 of the way down on the page to your far right that says "clone or download", click that button and you will be given two options. The easiest is to download zip and then once downloaded right click the file and extract the files. Once that is done open the folder and locate the index.html file, this file will load the game locally.
+
+## Player Controls 
+
+These are the player controls:
+
+Arrow Up key: Move player up
+Arrow Down key: Move player down
+Arrow Left key: Move player left
+Arrow Right key: Move player right
+
+## Object of the Game
+
+The object of the game is to reach the water. On your way to the water you'll notice giant bugs crossing the screen,
+don't get hit by a bug or your player will start back to the beginning. Upon reaching the water your player will start back to the beginning.
+
+## Next steps
+
+Upon cloning or coping these files you have your own frogger type game. Please feel free to add cool features and make it your own...have fun and happy coding!
---
+++ js/app.js
@@ -0,0 +1,117 @@
+// Entity class for both player and enemis to 
+class Entity {
+    constructor() {
+        this.sprite = 'images/';
+        this.x = 205;
+        this.y = 430;
+        // this.width = 100;
+        // this.height = 100;
+    }
+    
+    // Check for collisions.
+    checkCollisions() {
+       for (i = 0; i < allEnemies.length; i++) {
+           if (player.x < allEnemies[i].x + allEnemies[i].width/2 &&
+            player.x + player.width/2 > allEnemies[i].x && 
+            player.y < allEnemies[i].y + allEnemies[i].height/2 && 
+            player.height/2 + player.y > allEnemies[i].y){
+                console.log('ouch!')
+                player.x = 205;
+                player.y = 430;
+            }
+       }
+
+    }
+
+    // Make enemies show up on the board 
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+// Enemies our player must avoid
+class Enemy extends Entity{
+    constructor(x, y) {
+        super();
+        this.sprite += 'enemy-bug.png';
+        this.x = x;
+        this.y = y;
+        this.speed = 101;
+        this.board = this.speed * 5;
+        this.width = 50;
+        this.height = 50;
+    }
+
+    // Update enemis position
+    update(dt){
+        if (this.x < this.board) {
+            this.x += 150* dt;
+        }
+        else{
+            this.x =- this.speed;
+        }
+    }
+
+};
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+class Player extends Entity {
+    constructor(){
+        super();
+        this.sprite += 'char-boy.png'
+        this.height = 50;
+        this.width = 50;
+    }
+
+    // Udate the player's position
+    update(dt) {
+        this.speed * dt;
+
+        if (this.y < 20) {
+            this.x = 205;
+            this.y = 420;
+        }
+    }
+
+    // Move Player
+    handleInput(allowedKeys) {
+     
+        if (allowedKeys === 'left' && this.x > 5){
+            this.x -= 100;
+        }else if (allowedKeys === 'right' && this.x < 405){
+            this.x += 100;
+        } else if (allowedKeys === 'up' && this.y > 20) {
+            this.y -= 99;
+        }else if (allowedKeys === 'down' && this.y < 400) {
+            this.y += 100;
+       }
+       console.log(this.x, this.y);
+    }
+
+    
+};
+
+// Now instantiate your objects.
+const player = new Player();
+const bug1= new Enemy(-101, 55);
+const bug2 = new Enemy((-101 * 3), 133);
+const bug3 = new Enemy(-101 *5, 225);
+
+// Place all enemy objects in an array called allEnemies
+let allEnemies = [bug1, bug2, bug3];
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+    
+    player.handleInput(allowedKeys[e.keyCode]);
+});
